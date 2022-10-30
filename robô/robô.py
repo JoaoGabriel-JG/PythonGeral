af9b60fd323103367b89edbbe153a609c62c847b
João Gabriel <85807914+JoaoGabriel-JG@users.noreply.github.com>
2022-10-30 12:04:49 -0300

    att

--- robô/robô.py
+++ robô/robô.py
@@ -1,14 +1,23 @@
-import pyautogui
+import pyautogui, time
 
-pyautogui.PAUSE = 3
+pyautogui.PAUSE = 1.5
 pyautogui.press('win')
 pyautogui.write('Login.xlsx')
 pyautogui.press('enter')
+pyautogui.keyDown('alt')
+time.sleep(2)
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.keyUp('alt')
 
-pyautogui.click(x = 382, y = 334)
+pyautogui.click(x = 388, y = 291)
 pyautogui.write('Joao.gabriel')
 
-pyautogui.click(x = 356, y = 384)
+pyautogui.click(x = 412, y = 335)
 pyautogui.write('senhaforte@123')
 
 pyautogui.click(x = 340, y = 475)
+
