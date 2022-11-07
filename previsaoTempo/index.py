78fec1ca1ffe7fced64ac67c988c470302583c66
João Gabriel <joao.nitsus@gmail.com>
2022-11-07 10:55:02 -0300

    att

--- previsaoTempo/index.py
+++ previsaoTempo/index.py
@@ -4,7 +4,8 @@ from pprint import pprint   # Transformar o arquivo JSON em uma árvore facilita
 API_Key = '595d39bc8c5681fba199b98966a1da1d'
 
 city = input("Enter a city: ")
-base_url = "http://api.openweathermap.org/data/2.5/weather?appid="+API_Key+"&q="+city   # Resposta da API com os dados
+base_url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_Key}&lang=pt_br" # Resposta da API com os dados
 weather_data = requests.get(base_url).json()    # Transforma a resposta em um formato JSON
+temp = weather_data['main']['temp'] - 273.15
 
-pprint(weather_data)
+print(f'{temp:.2f} °C')
