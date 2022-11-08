0d8c6cc947983a4c6a26d5868ee794f2b03053da
João Gabriel <joao.nitsus@gmail.com>
2022-11-08 08:38:20 -0300

    att

--- pesquisaBin/index.py
+++ pesquisaBin/index.py
@@ -1,1 +1,8 @@
-# A pesquisa binária é 
+# A pesquisa binária é mais rapida que a normal pois não necessita percorrer todas as opções para achar a correta
+
+def native_search(l, target):
+    # exemplo: l = [1, 2, 3, 10, 12]
+    for i in range(len(l)):
+        if l[i] == target:
+            return i
+    return -1
