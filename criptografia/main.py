cbda26d8decf465120382df333122cecbf9dea32
João Gabriel <joao.nitsus@gmail.com>
2023-02-09 12:02:44 -0300

    att

--- criptografia/main.py
+++ criptografia/main.py
@@ -1,8 +1,18 @@
 from random import choice
+import json
 
 chars = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789_-;'
+dictionary = {}
 
 with open('criptografia/num.txt', 'r') as arquivo: 
-    for n in chars:
+    numero = arquivo.readlines()
+    for n in numero:
         char = choice(chars)
-              
+        chars = chars.replace(char, '')
+        n = n.replace('\n', '')
+        dictionary.update({ char: n })
+
+print(dictionary)
+
+# with open('leozinhoPVP.json', 'w') as arquivo:
+#     json.dump(dictionary, arquivo, ensure_ascii=False, indent=2)
