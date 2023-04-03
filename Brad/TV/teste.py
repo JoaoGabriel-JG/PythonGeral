f6f4908942ca9195e995692cd54deb95bde9158c
JoaoGabriel-JG <joao.nitsus@gmail.com>
2023-04-03 10:49:57 -0300

    att

--- Brad/TV/teste.py
+++
@@ -1,8 +0,0 @@
-from funcionalidades import *
-
-tv = Televisor ('Sony', 'Sony-123')
-controle = ControleRemoto(tv)
-controle.sintonizaCanal('SBT')
-controle.trocaCanal('SBT')
-
-print(tv.canal_atual)
